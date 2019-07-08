acb738cd60d2268b4b93f9f910c119a24f8941c7
devguy <gunawan.prasetio@ramayana.co.id>
2019-07-08 10:47:41 +0700

    tets

--- applications/resources/views/frontend/master/includes/footer.blade.php
+++ applications/resources/views/frontend/master/includes/footer.blade.php
@@ -90,8 +90,10 @@
                 <!-- Copywrite Text -->
                 <div class="col-12 col-md-12">
                     <div class="copywrite-text">
-											<p class="footer-text" style="text-align:center">www.jalinnusantara.com || RESPECTING THE ENVIRONMENT, MUTUAL COOPERATION AND SPORTSMANSHIP
-							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                      <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+
                     </div>
                 </div>
             </div>
